1cc73b9db9fb95c98e2366e07ecbef2b7d5dcde8
benjaminbaret <benjamin.baret@edu.ece.fr>
2020-11-27 23:28:51 +0100

    Ajout de commentaires

--- Classes/jouer.cpp
+++ Classes/jouer.cpp
@@ -8,15 +8,15 @@
 
 
 void jouer(User &joueur1, User &joueur2) {
-    if (tirageJoueur(joueur1, joueur2)) {
-        boucleJeux(joueur1, joueur2);
+    if (tirageJoueur(joueur1, joueur2)) { // On détermine ici qui commence
+        boucleJeux(joueur1, joueur2); // On appelle en conséquence les joueurs dans la boucle de jeu dans l'ordre tiré
 
     } else {
         boucleJeux(joueur2, joueur1);
     }
 }
 
-int tirageJoueur(const User &joueur1, const User &joueur2) {
+int tirageJoueur(const User &joueur1, const User &joueur2) { // Determination de quel joueur commence
     int commence;
     commence = rand() % (2);
     if (commence == 0) {
@@ -24,32 +24,34 @@ int tirageJoueur(const User &joueur1, const User &joueur2) {
     } else {
         std::cout << "C'est le joueur : " << joueur2.getNom() << " qui commence" << std::endl;
     }
-    return commence;
+    return commence; // on renvoi qui commence
 }
 
 
-void boucleJeux(User &premierJoueurPioche,User &secondJoueurPioche){
+void boucleJeux(User &premierJoueurPioche,User &secondJoueurPioche){ // on tourne dans cette boucle pour le déroulement du jeu
     int isEnd = 1;
+
+    // on appelle méthodes permettant de tirer les cartes enjeu
 
     premierJoueurPioche.tirerCarteEnjeu();
     secondJoueurPioche.tirerCarteEnjeu();
 
     do {
-        secondJoueurPioche.verificationIpCreature();
+        secondJoueurPioche.verificationIpCreature();                           // On regarde si les créatures du joueur on lieu de rester sur le plateau (ip>0)
         secondJoueurPioche.affichagePlateau(premierJoueurPioche);
         secondJoueurPioche.afficherResume();
-        isEnd = secondJoueurPioche.verificationFinJeu();
-        if (isEnd != 0) {
-            if (secondJoueurPioche.proposerCarte()) {
-                secondJoueurPioche.placer(premierJoueurPioche);
+        isEnd = secondJoueurPioche.verificationFinJeu();                        // On regarde si le joueur a encore des IP ou s'il a encore des créatures
+        if (isEnd != 0) {                                                       // Si ce n'est pas la fin du jeu
+            if (secondJoueurPioche.proposerCarte()) {                           // On propose de placer carte
+                secondJoueurPioche.placer(premierJoueurPioche);     // Si oui pour la placer, on la place (on proposera d'attaquer dans la méthode placer)
             } else
-                secondJoueurPioche.proposerAttaque(premierJoueurPioche);
+                secondJoueurPioche.proposerAttaque(premierJoueurPioche); // On propose sinon d'attaquer
 
         } else
-            premierJoueurPioche.echangeEnjeu(secondJoueurPioche);
+            premierJoueurPioche.echangeEnjeu(secondJoueurPioche); // Sinon, c'est la fin de jeu, on prend la carte "Enjeu" de l'adversaire
 
 
-        if (isEnd != 0) {
+        if (isEnd != 0) { //
             premierJoueurPioche.verificationIpCreature();
             premierJoueurPioche.affichagePlateau(secondJoueurPioche);
             premierJoueurPioche.afficherResume();
@@ -66,6 +68,6 @@ void boucleJeux(User &premierJoueurPioche,User &secondJoueurPioche){
                 secondJoueurPioche.echangeEnjeu(premierJoueurPioche);
 
         }
-    } while (isEnd);
+    } while (isEnd); // on repete tant que tous les joueurs on une créature (au moins) ou on assez de pv
 
 }
--- main.cpp
+++ main.cpp
@@ -30,51 +30,57 @@ int main(int argc, char **argv) {
     srand(time(NULL));
     std::string path = "../Classes/fichier.csv";
 
-    nouveauJoueur.positionFichier=0;
-
+    nouveauJoueur.positionFichier = 0;
 
 
     while (isEnd) {
-        Color(1,0);
-        std::cout<<"______  _                                                     _                     \n"
-                   "| ___ \\(_)                                                   | |                    \n"
-                   "| |_/ / _   ___  _ __ __   __ ___  _ __   _   _   ___      __| |  __ _  _ __   ___  \n"
-                   "| ___ \\| | / _ \\| '_ \\\\ \\ / // _ \\| '_ \\ | | | | / _ \\    / _` | / _` || '_ \\ / __| \n"
-                   "| |_/ /| ||  __/| | | |\\ V /|  __/| | | || |_| ||  __/   | (_| || (_| || | | |\\__ \\ \n"
-                   "\\____/ |_| \\___||_| |_| \\_/  \\___||_| |_| \\__,_| \\___|    \\__,_| \\__,_||_| |_||___/ \n"
-                   "                                                                                    \n"
-                   "                                                                                    "<<"\n"<<std::endl;
-        Color(14,0);
-        std::cout<<" (     (    (     (           (          (     (                )          \n"
-                   " )\\ )  )\\ ) )\\ )  )\\ )        )\\ )       )\\ )  )\\ )  (       ( /(   *   )  \n"
-                   "(()/( (()/((()/( (()/(    (  (()/( (    (()/( (()/(  )\\ )    )\\())` )  /(  \n"
-                   " /(_)) /(_))/(_)) /(_))   )\\  /(_)))\\    /(_)) /(_))(()/(   ((_)\\  ( )(_)) \n"
-                   "(_))_ (_)) (_))_|(_))_|_ ((_)(_)) ((_)  (_))_|(_))   /(_))_  _((_)(_(_())  \n"
-                   " |   \\|_ _|| |_  | |_ | | | |/ __|| __| | |_  |_ _| (_)) __|| || ||_   _|  \n"
-                   " | |) || | | __| | __|| |_| |\\__ \\| _|  | __|  | |    | (_ || __ |  | |    \n"
-                   " |___/|___||_|   |_|   \\___/ |___/|___| |_|   |___|    \\___||_||_|  |_|    \n"
-                   "                                                                           "<<std::endl;
+        Color(1, 0);
+        std::cout << "______  _                                                     _                     \n"
+                     "| ___ \\(_)                                                   | |                    \n"
+                     "| |_/ / _   ___  _ __ __   __ ___  _ __   _   _   ___      __| |  __ _  _ __   ___  \n"
+                     "| ___ \\| | / _ \\| '_ \\\\ \\ / // _ \\| '_ \\ | | | | / _ \\    / _` | / _` || '_ \\ / __| \n"
+                     "| |_/ /| ||  __/| | | |\\ V /|  __/| | | || |_| ||  __/   | (_| || (_| || | | |\\__ \\ \n"
+                     "\\____/ |_| \\___||_| |_| \\_/  \\___||_| |_| \\__,_| \\___|    \\__,_| \\__,_||_| |_||___/ \n"
+                     "                                                                                    \n"
+                     "                                                                                    " << "\n"
+                  << std::endl;
+        Color(14, 0);
+        std::cout << " (     (    (     (           (          (     (                )          \n"
+                     " )\\ )  )\\ ) )\\ )  )\\ )        )\\ )       )\\ )  )\\ )  (       ( /(   *   )  \n"
+                     "(()/( (()/((()/( (()/(    (  (()/( (    (()/( (()/(  )\\ )    )\\())` )  /(  \n"
+                     " /(_)) /(_))/(_)) /(_))   )\\  /(_)))\\    /(_)) /(_))(()/(   ((_)\\  ( )(_)) \n"
+                     "(_))_ (_)) (_))_|(_))_|_ ((_)(_)) ((_)  (_))_|(_))   /(_))_  _((_)(_(_())  \n"
+                     " |   \\|_ _|| |_  | |_ | | | |/ __|| __| | |_  |_ _| (_)) __|| || ||_   _|  \n"
+                     " | |) || | | __| | __|| |_| |\\__ \\| _|  | __|  | |    | (_ || __ |  | |    \n"
+                     " |___/|___||_|   |_|   \\___/ |___/|___| |_|   |___|    \\___||_||_|  |_|    \n"
+                     "                                                                           " << std::endl;
 
-        Color(11,0);
+        Color(11, 0);
 
 
-        donneesJoueurs = lectureDonnees(path); // Gestion d'erreur en cas d'echec de lecture ?
-        isEnd = gestionMenu(donneesJoueurs);
+        donneesJoueurs = lectureDonnees(path); // Recuperation donnees joueurs dans BDD (fichier texte)
+        isEnd = gestionMenu(donneesJoueurs); // On regarde ce que les utilisateurs souhaitent faire
 
         if (isEnd == 2) {
-            ajoutJoueurEnMemoire(path, donneesJoueurs);
+            ajoutJoueurEnMemoire(path, donneesJoueurs); // Permet de créer et ajouter en mémoire le joueur
 
         } else if (isEnd == 3) {
-            User* joueur1 = new User();
-            User* joueur2 = new User();
+            User *joueur1 = new User();
+            User *joueur2 = new User();
 
 
             if (donneesJoueurs.size() >= 2) {
 
                 reinitialiserJoueurs(*joueur1, *joueur2);
 
-                initialisationJoueur(*joueur1, donneesJoueurs, joueursCombattants(donneesJoueurs, nomJoueurComparaison));
-                initialisationJoueur(*joueur2, donneesJoueurs, joueursCombattants(donneesJoueurs, nomJoueurComparaison));
+                // on initialise le sobjets joueurs en fonction des données (nb cartes de chaque type, argent etc..) de chaque joueur
+
+                initialisationJoueur(*joueur1, donneesJoueurs,
+                                     joueursCombattants(donneesJoueurs, nomJoueurComparaison));
+                initialisationJoueur(*joueur2, donneesJoueurs,
+                                     joueursCombattants(donneesJoueurs, nomJoueurComparaison));
+
+                // on créé les pioches de chaque joueur afin de jouer
 
                 initialisationDeckPiocheJoueur(*joueur1);
                 initialisationDeckPiocheJoueur(*joueur2);
@@ -82,11 +88,10 @@ int main(int argc, char **argv) {
                 //On lance la partie ici
                 jouer(*joueur1, *joueur2);
 
-
+                //Enregistrement des données (qui ce sont vues modifiées) dans la BDD
                 enregistrementDonneesJoueurs(path, donneesJoueurs, *joueur1, *joueur2);
 
                 donneesJoueurs.clear();
-
 
 
                 delete joueur1;
@@ -97,22 +102,19 @@ int main(int argc, char **argv) {
                 std::cout << "Veuillez enregistrer au miniumum deux joueurs" << std::endl;
             }
 
-        } else if(isEnd==4){
+        } else if (isEnd == 4) {
             acheter(path, donneesJoueurs);
-        }
-        else if(isEnd==5){
-           affichageGraphique();
+        } else if (isEnd == 5) {
+            affichageGraphique(); // Présentation cartes de chaque joueur
         }
 
         nomJoueurComparaison.clear();
 
     }
-    std::cout<<"Merci d'avoir joue a Diffuse Fight !"<<std::endl;
-
+    std::cout << "Merci d'avoir joue a Diffuse Fight !" << std::endl;
 
 
     return 0;
-
 
 
 }
